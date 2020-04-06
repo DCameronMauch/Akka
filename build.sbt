6eb9fc6aa89f5ddae2c1c87ffc914c70717de5f0
D Cameron Mauch <cam@douglas-cameron.net>
2020-04-05 19:16:58 -0600

    actor compiles

--- build.sbt
+++ build.sbt
@@ -1,11 +1,13 @@
 ThisBuild / scalaVersion := "2.13.1"
-ThisBuild / organization := "com.github.DCameronMauch"
+ThisBuild / organization := "com.GitHub.DCameronMauch"
 
 lazy val mainDependencies: Seq[ModuleID] = Seq(
   "com.typesafe.akka" %% "akka-actor-typed" % "2.6.4"
 )
 
-lazy val testDependencies: Seq[ModuleID] = Seq.empty
+lazy val testDependencies: Seq[ModuleID] = Seq(
+  "org.scalatest" %% "scalatest" % "3.1.1"
+)
 
 lazy val allDependencies: Seq[ModuleID] = mainDependencies ++ testDependencies.map(_ % Test)
 
--- src/main/scala/com/GitHub/DCameronMauch/Akka/actor/Counter.scala
+++ src/main/scala/com/GitHub/DCameronMauch/Akka/actor/Counter.scala
@@ -1,16 +1,23 @@
 package com.GitHub.DCameronMauch.Akka.actor
 
 import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl._
 
 object Counter {
   sealed trait Message
   final case object Get extends Message
 
-  def apply(): Behavior[Message] = Behaviors.receive(
-    (context, message) =>
+  def apply(): Behavior[Message] =
+    Behaviors.setup(context => new Counter(context))
+}
+
+import Counter.{Get, Message}
+
+class Counter(context: ActorContext[Message]) extends AbstractBehavior[Message](context) {
+  def onMessage(message: Message): Behavior[Message] =
     message match {
-      case Get => println("got get message")
+      case Get =>
+        println("foo")
+        this
     }
-  )
 }
